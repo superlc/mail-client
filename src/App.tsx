2ed1c477adf2bdad8a5cdcdc1bc1b15a257cbb46
chaoluo(罗超) <chaoluo@tencent.com>
2023-11-02 22:47:01 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./pages/home";
+import Login from "./pages/login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
